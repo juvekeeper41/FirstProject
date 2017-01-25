fb9208e2738fd3b5fdc70b6a3463de1970c8b30a
juvekeeper41 <josb6385@740s-pl-114.local>
2017-01-25 09:48:26 -0700

    Built simple functions.
    nameCheck
    ageCheck
    etc

--- FirstProject/FirstController/FirstController.cpp
+++ FirstProject/FirstController/FirstController.cpp
@@ -11,7 +11,30 @@
 
 using namespace std;
 
-void start()
+void FirstController :: start()
 {
     cout << "C++ Rocks!" << endl;
+    int myAge = 17;
+    cout << "I am " << myAge << " years old." << endl;
+    cout << "\n";
+    
+    this->specialOutput();
+    
 }
+
+void FirstController:: specialOutput()
+{
+    cout << "How old are you?" << endl;
+    int yourAge;
+    cin >> yourAge;
+    cout << "Oh, so you are " << yourAge << " years old!"<< endl;
+    string myName;
+    cin.ignore();
+    cout << "Type in your name" << endl;
+    getline(cin, myName);
+    cout << "You typed: " << myName << endl;
+    cout << "Type in your name and age" << endl;
+    cin >> myName >> yourAge;
+    cout << "You typed: " << myName << " and " << yourAge << endl;
+}
+
